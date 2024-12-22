bfdbce384cb8f65850dc004ce58e86f8251f59a4
SOUMYA\ersau <ersaumyarout@gmail.com>
2024-12-22 11:37:30 +0530

    added movie query amd handler

---
+++ Movies.Application/Queries/GetMovieByDirectorNameQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Movies.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Queries
+{
+    public class GetMovieByDirectorNameQuery:IRequest<IEnumerable<MovieResponse>>
+    {
+        public string DirectorName { get; set; }
+        public GetMovieByDirectorNameQuery(string directorName )
+        {
+            DirectorName = directorName;
+        }
+    }
+}
